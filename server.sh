4d4a6cdc113cc6c7848da99ecfc8a93213a187d0
horiso0921 <horiso0921@gmail.com>
2022-02-05 03:34:39 +0000

    Modify：Pathを現在のものに修正

--- server.sh
+++ server.sh
@@ -1,15 +1,16 @@
+CUDA_LAUNCH_BLOCKING=1
 nohup python3 scripts/dialog.py data/sample/bin/ \
- --path /home/ubuntu/hobbyst/checkpoints/persona50k-flat_1.6B_33avog1i_4.16.pt \
+ --path /home/ubuntu/Platform/data/model/checkpoint_best.pt \
  --beam 30 \
  --min-len 10 \
  --source-lang src \
  --target-lang dst \
  --tokenizer space \
  --bpe sentencepiece \
- --sentencepiece-model /home/ubuntu/hobbyst/sentencepiece_models/sp_oall_32k.model \
+ --sentencepiece-model /home/ubuntu/Platform/data/dicts/sp_oall_32k.model \
  --no-repeat-ngram-size 3 \
  --nbest 30 \
  --sampling \
  --sampling-topp 0.9 \
  --temperature 1.0 \
- --show-nbest 5 > out3.log 2> error3.log &
+ --show-nbest 30 > out.log 2> error.log &

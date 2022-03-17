1b8749429e86bf711e8e8c60d49ba55395823c41
horiso0921 <horiso0921@gmail.com>
2022-03-17 22:50:18 +0900

    Modify：ディレクトリの位置を調整

--- scripts/page.py
+++ scripts/page.py
@@ -1,8 +1,8 @@
 STYLE_SHEET = "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.css"
 FONT_AWESOME = "https://use.fontawesome.com/releases/v5.3.1/js/all.js"
-with open("/home/ubuntu/Platform/css/page.css", "r") as t:
+with open("../css/page.css", "r") as t:
   CSS = t.read()
-with open("/home/ubuntu/Platform/js/page.js", "r") as t:
+with open("../js/page.js", "r") as t:
   JS = t.read()
 WEB_HTML = """<html>
     <link rel="stylesheet" href={} />
@@ -20,14 +20,10 @@ WEB_HTML = """<html>
                       <div class="media-content">
                         <div class="content">
                           <p>
-                            <strong>インストラクション</strong>
+                            <strong>対話のページ</strong>
                             <br>
-                            対話のページになります．<br>
-                            絵文字を入力しないでください．<br>
+                            システムと対話をするためのページです．<br>
                             対話をやり直す場合はブラウザを更新してください．<br>
-                            発話を入力してから，応答が返ってくるまで少し時間があります．<br>
-                            5秒以上たっても返信がない場合は クラウドワークスにて連絡をお願いします。<br>
-                            すべて終わったら対話をダウンロードするボタンが下に出ますので，クリックして，クラウドワークスのお仕事ページにて提出をお願いします．
                         </p>
                         </div>
                       </div>

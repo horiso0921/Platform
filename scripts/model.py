4899de53f3d925a51bbe6b80ca32482115f7a14e
horiso0921 <horiso0921@gmail.com>
2022-04-01 10:59:47 +0900

    Delete：不要なimport

--- scripts/model.py
+++ scripts/model.py
@@ -30,7 +30,6 @@ from fairseq.data import encoders
 from fairseq.token_generation_constraints import pack_constraints, unpack_constraints
 from fairseq_cli.generate import get_symbols_to_strip_from_output
 
-from fairseq.dataclass.configs import FairseqConfig
 from fairseq.dataclass.utils import convert_namespace_to_omegaconf
 
 SEPARATOR = "[SEP]"
